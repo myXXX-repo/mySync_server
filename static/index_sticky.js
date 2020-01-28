9a9d47d7bb46be5ed8465f17eba2b97337e22234
WolfHugo <1031629044@qq.com>
2020-01-29 00:26:35 +0800

    Update index_sticky.js

--- static/index_sticky.js
+++ static/index_sticky.js
@@ -1,62 +1,89 @@
-let getFormatedDateTime = function(){
+let getFormatedDateTime = function () {
     var date = new Date();
     var year = date.getFullYear();
-    var month = date.getMonth()+1;
-    if(month<10){
-        month="0"+month;
+    var month = date.getMonth() + 1;
+    if (month < 10) {
+        month = "0" + month;
     }
     var day = date.getDate();
-    if(day<10){
-        day="0"+day;
+    if (day < 10) {
+        day = "0" + day;
     }
     var hour = date.getHours();
-    if(hour<10){
-        hour="0"+hour;
+    if (hour < 10) {
+        hour = "0" + hour;
     }
     var min = date.getMinutes();
-    if(min<10){
-        min="0"+min;
+    if (min < 10) {
+        min = "0" + min;
     }
     var sec = date.getSeconds();
-    if(sec<10){
-        sec="0"+sec;
+    if (sec < 10) {
+        sec = "0" + sec;
     }
     return `${year}-${month}-${day} ${hour}:${min}:${sec}`;
 }
 
 new Vue({
-    el:"#input_panel",
-    data:{
-        title:"Default Title",
-        con:"",
-        time:"",
-        devName:"",
-        ip:""
+    el: "#input_panel",
+    data: {
+        title: "Default Title",
+        con: "",
+        time: "",
+        devName: "",
+        ip: ""
     },
-    methods:{
-        onBtnSubmitClick(){
-            console.log("123");
-            localStorage.setItem('devName',this.devName);
-            that=this
-            axios.post("/v2/Sticky/add",{
-                title:that.title,
-                con:that.con,
-                time:that.time,
-                devName:that.devName,
-                ip:that.ip
-            }).then(function(response){
-                console.log(response.data)
-            },function(err){
-                console.log(err)
+    methods: {
+        onBtnSubmitClick() {
+            localStorage.setItem('devName', this.devName);
+            let that = this;
+            axios({
+                method: "post",
+                url: "/v2/Sticky/add",
+                params: {
+                    title: that.title,
+                    con: that.con,
+                    time: that.time,
+                    devName: that.devName,
+                    ip: that.ip
+                }
+            }).then(function (response) {
+                console.log(response.data);
+            }).catch(function (e) {
+                console.log(err);
             });
         }
     },
-    created(){
-        that = this
+    created() {
+        let that = this;
         this.time = getFormatedDateTime();
         this.devName = localStorage.getItem('devName');
-        axios.get("/getip").then(function(request){
+        axios.get("/getip").then(function (request) {
             that.ip = request.data;
-        });
+            console.log(request.data);
+        }).catch(function (e) {
+            console.log(e);
+        })
     },
 });
+
+new Vue({
+    el: "#sticky_display_panel",
+    data: {
+        sticky_list: ""
+    },
+    methods: {
+
+    },
+    mounted() {
+        that = this;
+        axios({
+            method: 'get',
+            url: "/v2/Sticky/get"
+        }).then(function (response) {
+            that.sticky_list = response.data;
+        }).catch(function (err) {
+            console.log(err);
+        });
+    }
+})
