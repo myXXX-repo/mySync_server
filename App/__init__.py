630ce821a7a57aac8a6b065ac16ef6ac636e0e34
WolfHugo <1031629044@qq.com>
2020-02-06 23:20:47 -0800

    break settings up

--- App/__init__.py
+++ App/__init__.py
@@ -1,13 +1,14 @@
 from flask import Flask
 
 from App.ext import init_ext
+from App.settings import init_setting
 from App.views import init_view
 
 
 def create_app():
     app = Flask(__name__)
 
-    # init_ext
+    init_setting(app)
     init_ext(app)
     init_view(app)
     return app
--- App/settings.py
+++ App/settings.py
@@ -2,3 +2,4 @@ def init_setting(app):
     # db uri db_type+driver://username:password@host:port/dbname
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../data/data.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    return app
