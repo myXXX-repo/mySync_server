cc8d9b0aeefd878253e61161a9c57bcca68f5fd5
WolfHugo <1031629044@qq.com>
2020-02-07 23:51:46 -0800

    break settings up

--- App/models.py
+++ App/models.py
@@ -3,14 +3,82 @@ from flask_sqlalchemy import SQLAlchemy
 from App.ext import db
 
 
-# class model(db.Model):
-#     pass
-
-
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(16))
 
-    def save(self):
+    def add_save(self):
         db.session.add(self)
         db.session.commit()
+
+
+class Git(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    repo_name = db.Column(db.String(1024))
+    remote_addr = db.Column(db.String(1024))
+    local_addr = db.Column(db.String(1024))
+    branch = db.Column(db.String(1024))
+    last_update_time = db.Column(db.String(1024))
+    last_check_time = db.Column(db.String(1024))
+    depth = db.Column(db.Integer)
+    enabled = db.Column(db.Boolean)
+
+    def add_save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+class Sticky(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Column(db.String(1024))
+    con = db.Column(db.String(1024))
+    devName = db.Column(db.String(1024))
+    ip = db.Column(db.String(1024))
+
+    def add_save(self):
+        db.session.add(self)
+        db.session.commit()
+
+
+# class ToDoList(db.Model):
+#     def add_save(self):
+#         db.session.add(self)
+#         db.session.commit()
+#
+#     pass
+#
+#
+# class Statistic(db.Model):
+#     def add_save(self):
+#         db.session.add(self)
+#         db.session.commit()
+#
+#     pass
+
+
+if __name__ == '__main__':
+    u = User()
+    u.add_save()
+
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(16))
+#
+#     def add_save(self):
+#         db.session.add(self)
+#         db.session.commit()
+#
+#
+# class Git(db.Model):
+#     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+#     repo_name = db.Column(db.String(1024))
+#     remote_addr = db.Column(db.String(1024))
+#     local_addr = db.Column(db.String(1024))
+#     branch = db.Column(db.String(1024))
+#     last_update_time = db.Column(db.String(1024))
+#     last_check_time = db.Column(db.String(1024))
+#     depth = db.Column(db.Integer)
+#
+#     def add_save(self):
+#         db.session.add(self)
+#         db.session.commit()
--- App/settings.py
+++ App/settings.py
@@ -1,5 +1,6 @@
 def init_setting(app):
     # db uri db_type+driver://username:password@host:port/dbname
+    # mysql+pymysql://root:password@localhost:3306/database_name
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../data/data.db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     return app
