105258a09c43b8268e29560447d3754c2573e2e5
alone-wolf <1031629044@qq.com>
2020-05-01 23:57:17 +0800

    update

--- manage.py
+++ manage.py
@@ -1,8 +1,16 @@
 from mySync import create_server
 from flask_script import Manager
+from os.path import exists
+from os.path import isdir
+from os import mkdir
 
 server = create_server()
 manager = Manager(server)
 
 if __name__ == '__main__':
+
+    if not (exists("data") and isdir("data")):
+        print("data folder not exists, creating...")
+        mkdir("data")
+
     manager.run()
--- mySync/apps/__init__.py
+++ mySync/apps/__init__.py
@@ -3,12 +3,14 @@ from mySync.apps.Main import init_app_Main
 from mySync.apps.Markdown import init_app_Markdown
 from mySync.apps.Sticky import init_app_Sticky
 from mySync.apps.VisitCounter import init_app_VisitCounter
+from mySync.apps.ApplicationData import init_app_ApplicationData
 
 
 def init_app(server):
-    init_app_Git(server)
+    # init_app_Git(server)
     init_app_Main(server)
-    init_app_Markdown(server)
+    # init_app_Markdown(server)
     init_app_Sticky(server)
-    init_app_VisitCounter(server)
+    # init_app_VisitCounter(server)
+    init_app_ApplicationData(server)
     return server
