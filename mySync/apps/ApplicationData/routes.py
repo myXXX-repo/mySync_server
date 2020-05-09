9e8460a825bb9247a65e1d6ff9709f9102428998
alone-wolf <1031629044@qq.com>
2020-05-09 15:50:17 +0800

    update app/ApplicationData

--- mySync/apps/ApplicationData/routes.py
+++ mySync/apps/ApplicationData/routes.py
@@ -1,6 +1,8 @@
 import time
 
 from flask import Blueprint, request, abort, jsonify
+from flask_restful import Resource, Api
+
 from mySync.apps.ApplicationData.models import ApplicationData
 from mySync.apps.ApplicationData.models import add_ones
 from mySync.apps.ApplicationData.models import del_all
@@ -13,21 +15,52 @@ from mySync.common.libs import jsonEncode
 
 from mySync.common.access_token_check import check_access_token
 
-ApplicationData_routes = Blueprint('ApplicationData_routes', __name__)
-
 allowed_app_name = ['com.wh.mydeskclock', 'com.wh.myconan']
 
 get_now_milli_time = lambda: int(time.time() * 1000)
 
+ApplicationData_routes = Blueprint('ApplicationData_routes', __name__)
 
-# id app_name key_value create_time update_time
-
-# http://ip:port/app/ApplicationData/v1.0/test GET   for testing
-# http://ip:port/app/ApplicationData/v1.0/all_data GET    get all data from sql
-# http://ip:port/app/ApplicationData/v1.0/data/<string:app_name> get   all data by
+AppData_api = Api(ApplicationData_routes)
 
 
-# 测试连接
+class AppData_all(Resource):
+    @check_access_token
+    def get(self):
+        return app_data_get_all_data()
+
+    @check_access_token
+    def delete(self):
+        return app_data_delete_all_data()
+
+
+class AppData_all_short_name(AppData_all):
+    pass
+
+
+class AppData_app(Resource):
+    @check_access_token
+    def get(self, app_name):
+        return app_data_get_app_data(app_name)
+
+    @check_access_token
+    def post(self, app_name):
+        return app_data_post_data(app_name)
+
+    @check_access_token
+    def delete(self, app_name):
+        return app_data_delete_app_data(app_name)
+
+
+class AppData_app_short_name(AppData_app):
+    pass
+
+
+AppData_api.add_resource(AppData_all, "/app/ApplicationData/v1.0/data_all")
+AppData_api.add_resource(AppData_all_short_name, "/app/AppData/v1.0/data_all")
+AppData_api.add_resource(AppData_app, "/app/ApplicationData/v1.0/data/<string:app_name>")
+AppData_api.add_resource(AppData_app_short_name, "/app/AppData/v1.0/data/<string:app_name>")
+
 
 @ApplicationData_routes.route('/app/ApplicationData/v1.0/test', methods=['GET'])
 @check_access_token
@@ -35,112 +68,75 @@ def test():
     return "server is fine"
 
 
-# get delete 全部数据
-@ApplicationData_routes.route('/app/ApplicationData/v1.0/all_data', methods=['GET', 'DELETE'])
-@check_access_token
-def get_delete_all_data():
-    request_method = request.method
-    if request_method == 'GET':
-        data_all = []
-        for i in get_all():
-            data = {
-                "id": i.id,
-                "app_name": i.app_name,
-                "key_value": i.key_value,
-                "create_time": i.create_time,
-                "update_time": i.update_time
-            }
-            data_all.append(data)
-        return jsonEncode(data_all)
-    elif request_method == 'DELETE':
-        del_all()
-        return "del all done"
+def app_data_get_all_data():
+    data_all = []
+    for i in get_all():
+        data = {
+            "id": i.id,
+            "app_name": i.app_name,
+            "key_value": i.key_value,
+            "create_time": i.create_time,
+            "update_time": i.update_time
+        }
+        data_all.append(data)
+    return jsonEncode(data_all)
 
 
-# get post delete 指定app的全部数据
-@ApplicationData_routes.route('/app/ApplicationData/v1.0/data/<string:app_name>', methods=['GET', 'POST', 'DELETE'])
-@check_access_token
-def get_post_delete_data(app_name):
-    # if app_name not in allowed_app_name:
-    #     print(app_name)
-    #     abort(403)
+def app_data_delete_all_data():
+    del_all()
+    return "del all done"
 
-    request_method = request.method
-    # 获取 某app的全部数据
-    if request_method == 'GET':
-        data_all = []
-        for i in get_data_by_app_name(app_name):
-            data = {
-                "id": i.id,
-                "app_name": i.app_name,
-                "key_value": i.key_value,
-                "create_time": i.create_time,
-                "update_time": i.update_time
-            }
-            data_all.append(data)
-        return jsonify(data_all)
 
-    # 上传某app的全部数据
-    elif request_method == 'POST':
-        request_json = request.json
+def app_data_get_app_data(app_name):
+    data_all = []
+    for i in get_data_by_app_name(app_name):
+        data = {
+            "id": i.id,
+            "app_name": i.app_name,
+            "key_value": i.key_value,
+            "create_time": i.create_time,
+            "update_time": i.update_time
+        }
+        data_all.append(data)
+    return jsonify(data_all)
 
-        applicationData_all = get_all()
-        applicationData_all_app_name = [i.app_name for i in applicationData_all]
-        if app_name in applicationData_all_app_name:
-            msg = "app_name exists, update data"
-            print(msg)
-            request_json = str(request.json)
-            data = get_data_by_app_name(app_name=app_name)
-            if len(data) != 1:
-                abort(500)
-            for i in data:
-                print(i.id)
-                print(i.app_name)
-                print(i.key_value)
-                print(i.create_time)
-                print(i.update_time)
-                i.key_value = request_json
-                i.update_time = get_now_milli_time()
 
-                modify_one_by_node(i)
-        else:
-            msg = "reg new app_name, add data"
-            print(msg)
-            timeNow = get_now_milli_time()
-            app_data = ApplicationData()
-            app_data.key_value = str(request_json)
-            app_data.app_name = app_name
-            app_data.create_time = timeNow
-            app_data.update_time = timeNow
+def app_data_post_data(app_name):
+    request_json = request.json
 
-            add_ones([app_data])
-        return msg
-    elif request_method == 'DELETE':
-        del_one_by_app_name(app_name=app_name)
-        return "del " + app_name + " all data done"
+    applicationData_all = get_all()
+    applicationData_all_app_name = [i.app_name for i in applicationData_all]
+    if app_name in applicationData_all_app_name:
+        msg = "app_name exists, update data"
+        print(msg)
+        request_json = str(request.json)
+        data = get_data_by_app_name(app_name=app_name)
+        if len(data) != 1:
+            abort(500)
+        for i in data:
+            print(i.id)
+            print(i.app_name)
+            print(i.key_value)
+            print(i.create_time)
+            print(i.update_time)
+            i.key_value = request_json
+            i.update_time = get_now_milli_time()
 
-# @ApplicationData_routes.route('/app/ApplicationData/v1.0/delete', methods=['DELETE'])
-# def del_all_data():
-#     del_all()
-#     return "del all done"
-#
-#
-# @ApplicationData_routes.route('/app/ApplicationData/v1.0/<string:app_name>/delete', methods=['DELETE'])
-# def del_data_by_app_name(app_name):
-#     del_one_by_app_name(app_name)
-#     return "del " + app_name + " all data done"
+            modify_one_by_node(i)
+    else:
+        msg = "reg new app_name, add data"
+        print(msg)
+        timeNow = get_now_milli_time()
+        app_data = ApplicationData()
+        app_data.key_value = str(request_json)
+        app_data.app_name = app_name
+        app_data.create_time = timeNow
+        app_data.update_time = timeNow
 
-# def itemInDB(data_all, data_item):
-#     data_all_app_name = []
-#     data_all_data_key = []
-#     for i in data_all:
-#         data_all_app_name.append(i['app_name'])
-#         data_all_data_key.append(i['data_key'])
-#
-#     if data_item['app_name'] not in data_all_app_name:
-#         return False
-#     elif data_item['data_key'] not in data_all_data_key:
-#         return False
-#     else:
-#         # update data
-#         return True
+        add_ones([app_data])
+    return msg
+
+
+def app_data_delete_app_data(app_name):
+    del_one_by_app_name(app_name=app_name)
+    return "del " + app_name + " all data done"
